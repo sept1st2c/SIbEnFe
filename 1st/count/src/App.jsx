8a5ade823e590020b52a706fd2dc354cc69b7ad9
sept1st2c <sept1st2c@gmail.com>
2024-06-05 16:16:21 +0530

    memo and key

--- 1st/count/src/App.jsx
+++ 1st/count/src/App.jsx
@@ -1,26 +1,57 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [name, setName] = useState("Shubh");
+let counter = 4;
 
-  function updateName() {
-    setName("the " + Math.random());
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Gym",
+      description: "Go to Gym",
+    },
+    {
+      id: 2,
+      title: "gg",
+      description: "Go ggg Gym",
+    },
+    {
+      id: 3,
+      title: "Gyhhm",
+      description: "hhhh to Gym",
+    },
+  ]);
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: counter++,
+        title: Math.random(),
+        description: Math.random(),
+      },
+    ]);
   }
 
   return (
-    <>
-      <button onClick={updateName}>Click</button>
-      <Header title={name}></Header>
-      <Header title={"ola hu"}></Header>
-    </>
+    <div>
+      <button onClick={addTodo}>add todo</button>
+      {todos.map((todo) => (
+        <ToDo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
+    </div>
   );
 }
 
-function Header({ title }) {
-  return <div>{title}</div>;
+function ToDo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 }
 
 export default App;
