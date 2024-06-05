adcb3c9e29b70c03593c8053a6587f14c664aa04
sept1st2c <sept1st2c@gmail.com>
2024-06-05 16:53:38 +0530

    use effect usage with interval

--- 1st/count/src/App.jsx
+++ 1st/count/src/App.jsx
@@ -1,21 +1,34 @@
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-let counter = 4;
-
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos").then(async function (res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      });
+    }, 5000);
+  }, []);
   return (
     <div>
-      <CardWrapper>hi there</CardWrapper>
+      {todos.map((todo) => (
+        <ToDo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
     </div>
   );
 }
 
-function CardWrapper({ children }) {
+function ToDo({ title, description }) {
   return (
-    <div style={{ border: "2px solid black", padding: 20 }}>{children}</div>
+    <div>
+      <h3>{title}</h3>
+      <h5>{description}</h5>
+    </div>
   );
 }
 
