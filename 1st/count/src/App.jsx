d648a7166f375ae6dcd9b6715a65a05c1f3e8b7b
sept1st2c <sept1st2c@gmail.com>
2024-06-05 16:33:30 +0530

    wrapper class learnt

--- 1st/count/src/App.jsx
+++ 1st/count/src/App.jsx
@@ -6,51 +6,16 @@ import "./App.css";
 let counter = 4;
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Gym",
-      description: "Go to Gym",
-    },
-    {
-      id: 2,
-      title: "gg",
-      description: "Go ggg Gym",
-    },
-    {
-      id: 3,
-      title: "Gyhhm",
-      description: "hhhh to Gym",
-    },
-  ]);
-
-  function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        id: counter++,
-        title: Math.random(),
-        description: Math.random(),
-      },
-    ]);
-  }
-
   return (
     <div>
-      <button onClick={addTodo}>add todo</button>
-      {todos.map((todo) => (
-        <ToDo key={todo.id} title={todo.title} description={todo.description} />
-      ))}
+      <CardWrapper>hi there</CardWrapper>
     </div>
   );
 }
 
-function ToDo({ title, description }) {
+function CardWrapper({ children }) {
   return (
-    <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
-    </div>
+    <div style={{ border: "2px solid black", padding: 20 }}>{children}</div>
   );
 }
 
