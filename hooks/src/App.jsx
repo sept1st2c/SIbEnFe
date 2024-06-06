7111f382df23e58ff6cbfbe75e07f71683a5574d
sept1st2c <sept1st2c@gmail.com>
2024-06-07 05:25:06 +0530

    new project

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -4,49 +4,34 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [ID, setID] = useState(5);
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  let count = 0;
+  for (let i = 1; i <= inputValue; i++) {
+    count = count + i;
+  }
 
   return (
     <div>
+      <input
+        onChange={function (e) {
+          setInputValue(e.target.value);
+        }}
+        placeholder={"Find sum from 1 to n"}
+      ></input>
+      <br />
+      Sum from 1 to {inputValue} is {count}
+      <br />
       <button
         onClick={() => {
-          setID(1);
+          setCounter(counter + 1);
         }}
       >
-        1
+        Counter ({counter})
       </button>
-      <button
-        onClick={() => {
-          setID(2);
-        }}
-      >
-        2
-      </button>
-      <button>3</button>
-      <button>4</button>
-      <Todo id={ID} />
     </div>
   );
 }
 
-function Todo({ id }) {
-  const [todo, setTodo] = useState({});
-
-  useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todo?id=" + id).then(async function (
-      res
-    ) {
-      const json = await res.json();
-      setTodo(json.todo);
-      console.log(id);
-    });
-  }, [id]);
-
-  return (
-    <div>
-      <h1>{todo.title}</h1>
-      <h4>{todo.description}</h4>
-    </div>
-  );
-}
 export default App;
