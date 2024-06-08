58fa5a037f02f9dd6aeaf1546735f403ecade959
sept1st2c <sept1st2c@gmail.com>
2024-06-09 04:47:38 +0530

    stuffs

--- easy-bits/src/App.jsx
+++ easy-bits/src/App.jsx
@@ -1,11 +1,50 @@
 import { useState } from "react";
+import {
+  jobsAtom,
+  messagingAtom,
+  networkAtom,
+  notificationsAtom,
+  totalNotificationSelector,
+} from "./atoms";
+import { useMemo } from "react";
 
 import "./App.css";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+  );
+}
 
-  return <div></div>;
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationsAtomCount = useRecoilValue(notificationsAtom);
+  const messagingAtomCount = useRecoilValue(messagingAtom);
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+  // const totalNotificationCount = useMemo(() => {
+  //   return networkNotificationCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount;
+  // }, [networkNotificationCount, jobsAtomCount, notificationsAtomCount, messagingAtomCount])
+
+  return (
+    <>
+      <button>Home</button>
+
+      <button>
+        My network (
+        {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
+      </button>
+      <button>Jobs {jobsAtomCount}</button>
+      <button>Messaging ({messagingAtomCount})</button>
+      <button>Notifications ({notificationsAtomCount})</button>
+
+      <button>Me ({totalNotificationCount})</button>
+    </>
+  );
 }
 
 export default App;
