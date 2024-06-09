3d47f7ca2e288de6b41a12979c6d1229904996ea
sept1st2c <sept1st2c@gmail.com>
2024-06-09 05:50:00 +0530

    if u r reading this im sorry

--- tailwind_prac/src/App.jsx
+++ tailwind_prac/src/App.jsx
@@ -8,7 +8,19 @@ function App() {
 
   return (
     <>
-      <div style={{ display: "flex" }}></div>
+      {/* <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <div style={{ backgroundColor: "yellow" }}>hi</div>
+        <div>hi</div>
+        <div style={{ backgroundColor: "yellow" }}>hi</div>
+        <div>hi</div>
+      </div> */}
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        <div className="bg-orange-700">hi</div>
+        <div>hi</div>
+        <div className="bg-orange-700">hi</div>
+        <div>hi</div>
+      </div>
     </>
   );
 }
